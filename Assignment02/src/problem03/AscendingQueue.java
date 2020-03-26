c29c684dd5f2ba675b4b0fe0a0762be775a5154a
DELL <DELL@DESKTOP-PNBGS9O>
2020-03-26 15:44:03 +0530

    create field of queue like rear,front,size and contructor

--- Assignment02/src/problem03/AscendingQueue.java
+++ Assignment02/src/problem03/AscendingQueue.java
@@ -1,5 +1,13 @@
 package problem03;
 
 public class AscendingQueue {
-
+	    private int[] data;
+	    private int front,rear;
+	    private int size;
+	    public AscendingQueue(int size) {
+	    	   this.size=size;
+		        data =new int[this.size];
+		        front=0;
+		        rear=-1;
+	    }
 }
