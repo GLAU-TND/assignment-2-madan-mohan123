ce4f0734b056cf5e555d67e3ac8464273e34fa74
DELL <DELL@DESKTOP-PNBGS9O>
2020-03-26 15:56:16 +0530

    print queue

--- Assignment02/src/problem03/AscendingQueueMain.java
+++ Assignment02/src/problem03/AscendingQueueMain.java
@@ -3,8 +3,17 @@ package problem03;
 public class AscendingQueueMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		AscendingQueue ar=new AscendingQueue(5);
+        ar.enqueue(56);
+       ar.enqueue(79);
+       ar.enqueue(33);
+        ar.enqueue(90);
+        ar.enqueue(30);
+    
+      ar.print();
+      ar.dequeue();
+      ar.print();
 	}
 
 }
