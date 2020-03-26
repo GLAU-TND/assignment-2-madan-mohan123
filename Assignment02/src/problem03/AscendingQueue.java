c67321fffc2a5895e86bbd67a3c34eb4814198a0
DELL <DELL@DESKTOP-PNBGS9O>
2020-03-26 15:45:34 +0530

    create enqueue method and isfull

--- Assignment02/src/problem03/AscendingQueue.java
+++ Assignment02/src/problem03/AscendingQueue.java
@@ -10,4 +10,25 @@ public class AscendingQueue {
 		        front=0;
 		        rear=-1;
 	    }
+	    public boolean enqueue(int element) {
+	    	 boolean response=false;
+		        if(!isFull()){
+		            data[rear+1]=element;
+		            rear++;
+		            response=true;
+		           // sorting(); 
+		        }
+		        else {
+		        	System.out.println("overflow at:"+element);
+		        }
+		        return response;
+		}
+	    boolean isFull(){
+	    	   boolean response=false;
+		        if(rear==size-1){
+		            response=true;
+		        }
+		        return response;
+	    }
+
 }
